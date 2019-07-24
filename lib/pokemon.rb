2325ed543c2cd7f8e2a244a5690f0bbba24312ca
github username <github email address>
2019-07-23 20:21:53 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+    attr_accessor :name, :type, :db, :id
+
+    def initialize(keyword)
+         
+    end 
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type) VALUES (?, ?)
+            SQL
+
+            DB[:conn].execute(sql, self.name, self.type)
+            @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end 
 end
