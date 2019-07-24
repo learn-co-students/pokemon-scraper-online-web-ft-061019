7e065e97aef31b3815fa976ff2e68416838a981f
github username <github email address>
2019-07-23 20:23:09 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,11 @@
 class Pokemon
     attr_accessor :name, :type, :db, :id
 
-    def initialize(keyword)
+    def initialize(name, type, db, id)
+        @name = name
+        @type = type
+        @db = db
+        @id = id 
          
     end 
 
